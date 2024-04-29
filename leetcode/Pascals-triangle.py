3ebb8d6762d516a9f634c5a1b85174b92d6dcbff
thais <thaiscamachoo@gmail.com>
2024-04-29 17:39:23 -0300

    up

---
+++ leetcode/Pascals-triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+p = [7,6,4,3,1]
+
+def pascals(n):
+    triangle = [[1], [1, 1]]
+    if(n == 1):
+        return [triangle[0]]
+    if(n == 2):
+        return [triangle[1]]
+    for i in range(3, n+1):
+        line = [1]
+        for j in range(2, i):
+            line.append(triangle[i-2][j-2] + triangle[i-2][j-1])
+        line.append(1)
+        triangle.append(line)
+    return triangle
+
+
+def main():
+    x = pascals(5)
+    print(x)
+
+if __name__ == "__main__":
+    main()
+
