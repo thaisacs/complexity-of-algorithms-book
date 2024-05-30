b3505d14da78614f44a02208a86ef008e442e0a4
thais <thaiscamachoo@gmail.com>
2024-05-30 01:05:58 -0300

    first 210

--- leetcode/210.cpp
+++ leetcode/210.cpp
@@ -8,22 +8,23 @@ struct node {
   int s, f;
 };
 
-void dfs_visit(int u, node* V, vector<int>* adj) {
+void dfs_visit(int u, node* V, vector<int>* adj, vector<int>& output) {
   time_++;
   V[u].s = time_;
   for(int i = 0; i < adj[u].size(); i++) {
     if(V[adj[u][i]].s == -1){
-      dfs_visit(adj[u][i], V, adj);
+      dfs_visit(adj[u][i], V, adj, output);
     }
   }
+  output.push_back(u);
   time_++;
   V[u].f = time_;
 }
 
-void dfs(int numCourses, node* V, vector<int>* adj, stack<int>& Stack) {
-  for(int i = 0; i < numCourses; i++) {
-    if(V[i].s == -1) {
-      dfs_visit(i, V, adj);
+void dfs(vector<int> input, node* V, vector<int>* adj, vector<int>& output) {
+  for(int i = input.size() - 1; i >= 0; i--) {
+    if(V[input[i]].s == -1) {
+      dfs_visit(input[i], V, adj, output);
     }
   }
 }
@@ -31,10 +32,12 @@ void dfs(int numCourses, node* V, vector<int>* adj, stack<int>& Stack) {
 void findOrder(int numCourses, vector<vector<int>>& prerequisites) {
   vector<int> adj[numCourses];
   vector<int> adjR[numCourses];
+  vector<int> input;
   node V[numCourses];
-  for(int i = 0; i < numCourses; i++) {
+  for(int i = numCourses - 1; i >= 0; i--) {
     V[i].s = -1;
     V[i].f = -1;
+    input.push_back(i);
   }
   for(int i = 0; i < prerequisites.size(); i++) {
     int u = prerequisites[i][0];
@@ -42,17 +45,28 @@ void findOrder(int numCourses, vector<vector<int>>& prerequisites) {
     adj[u].push_back(v);
     adjR[v].push_back(u);
   }
-  stack<int> Stack;
-  dfs(numCourses, V, adj, Stack);
-  //dfs(numCourses, V, adjR);
 
+  vector<int> output2;
+  vector<int> output;
+  dfs(input, V, adj, output2);
   for(int i = 0; i < numCourses; i++) {
-    cout << V[i].s << " " << V[i].f << endl;
+    V[i].s = -1;
+    V[i].f = -1;
   }
+  dfs(output2, V, adjR, output);
+
+  cout << "[";
+  for(int i = output.size()-1; i > 0; i--) {
+    cout << output[i] << ",";
+  }
+  if(output.size())
+    cout << output[0];
+  cout << "]";
+  cout << endl;
 }
 
 int main() {
-  int numCourses = 2;
+  int numCourses = 4;
   vector<vector<int>> prerequisites;
 
   vector<int> value1;
@@ -60,27 +74,22 @@ int main() {
   value1.push_back(0);
   prerequisites.push_back(value1);
 
-  //vector<int> value2;
-  //value2.push_back(0);
-  //value2.push_back(2);
-  //prerequisites.push_back(value2);
+  vector<int> value2;
+  value2.push_back(2);
+  value2.push_back(0);
+  prerequisites.push_back(value2);
 
-  //vector<int> value3;
-  //value3.push_back(1);
-  //value3.push_back(0);
-  //prerequisites.push_back(value3);
+  vector<int> value3;
+  value3.push_back(3);
+  value3.push_back(1);
+  prerequisites.push_back(value3);
+
+  vector<int> value4;
+  value4.push_back(3);
+  value4.push_back(2);
+  prerequisites.push_back(value4);
 
   findOrder(numCourses, prerequisites);
-
-  //vector<int> value4;
-  //value4.push_back(3);
-  //value4.push_back(2);
-  //prerequisites.push_back(value4);
-  //if(canFinish(numCourses, prerequisites)) {
-  //  cout << "true" << endl;
-  //}else {
-  //  cout << "false" << endl;
-  //}
 
   return 0;
 }
